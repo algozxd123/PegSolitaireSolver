84a1b68c328c19ec8521d7d3ddcaffb331dd6845
Gabriel Pereira <gabrielferreira5247@gmail.com>
2020-11-06 08:38:18 -0300

    Exploiting board symmetry to increase speed

--- PegSolitaireSolver/Origem.cpp
+++ PegSolitaireSolver/Origem.cpp
@@ -4,6 +4,7 @@
 #include <fstream>
 #include <vector>
 #include <string>
+#include <chrono>
 
 using namespace std;
 
@@ -176,24 +177,79 @@ void reverseMove(RESTAUM& ru, MOVIMENTO& movimento) {
 		}
 }
 
-int solve(RESTAUM& ru, vector<MOVIMENTO>& solucao, int& nodes) {
+int verifySmallestString(vector<int>& vector_one, vector<int>& vector_two) {
+	for (int i = 0; i < vector_one.size(); i++) {
+		if (vector_one[i] != vector_two[i]) {
+			if (vector_one[i] == 1) return 0;
+			else return 1;
+		}
+	}
+}
+
+vector<int> getBoardsVariations(RESTAUM& ru, vector<vector<int>>& tabuleiros_verificados) {
+	vector<int> matrix_to_vector[8];
+
+	for (int i = 0; i < ru.n; i++) {
+		for (int j = 0; j < ru.m; j++) {
+			if (ru.tabuleiro[i][j] != -1) matrix_to_vector[0].push_back(ru.tabuleiro[i][j]);
+			if (ru.tabuleiro[ru.n - i - 1][j] != -1) matrix_to_vector[1].push_back(ru.tabuleiro[ru.n - i - 1][j]);
+			if (ru.tabuleiro[i][ru.m - j - 1] != -1) matrix_to_vector[2].push_back(ru.tabuleiro[i][ru.m - j - 1]);
+			if (ru.tabuleiro[ru.n - i - 1][ru.m - j - 1] != -1) matrix_to_vector[3].push_back(ru.tabuleiro[ru.n - i - 1][ru.m - j - 1]);
+			if (ru.tabuleiro[j][i] != -1) matrix_to_vector[4].push_back(ru.tabuleiro[j][i]);
+			if (ru.tabuleiro[j][ru.n - i - 1] != -1) matrix_to_vector[5].push_back(ru.tabuleiro[j][ru.n - i - 1]);
+			if (ru.tabuleiro[ru.m - j - 1][i] != -1) matrix_to_vector[6].push_back(ru.tabuleiro[ru.m - j - 1][i]);
+			if (ru.tabuleiro[ru.m - j - 1][ru.n - i - 1] != -1) matrix_to_vector[7].push_back(ru.tabuleiro[ru.m - j - 1][ru.n - i - 1]);
+		}
+	}
+
+	vector<int> menor_vector = matrix_to_vector[0];
+
+	for (int i = 1; i < 8; i++) {
+		if (verifySmallestString(menor_vector, matrix_to_vector[i])) {
+			menor_vector = matrix_to_vector[i];
+		}
+	}
+
+	return menor_vector;
+}
+
+int verifyBoardsVerified(RESTAUM& ru, vector<vector<int>>& tabuleiros_verificados) {
+
+	vector<int> matrix_to_vector = getBoardsVariations(ru,tabuleiros_verificados);
+
+	for (int i = 0; i < tabuleiros_verificados.size(); i++) {
+		if (tabuleiros_verificados[i] == matrix_to_vector) {
+			return 0;
+		}
+	}
+	
+	return 1;
+}
+
+int solve(RESTAUM& ru, vector<MOVIMENTO>& solucao, vector<vector<int>>& tabuleiros_verificados, int& nodes) {
 	nodes++;
 	if (countPieces(ru) == 1) {
 		return 1;
 	}else {
-		vector<MOVIMENTO> movimentos_possiveis = getValidMoves(ru);
-		if (movimentos_possiveis.size()) {
-			for (int i = 0; i < movimentos_possiveis.size(); i++) {
-				makeMove(ru, movimentos_possiveis[i]);
-				int result = solve(ru, solucao, nodes);
-				if (result) {
-					solucao.push_back(movimentos_possiveis[i]);
-					return 1;
-				}else {
-					reverseMove(ru, movimentos_possiveis[i]);
+		if (verifyBoardsVerified(ru, tabuleiros_verificados)) {
+			vector<MOVIMENTO> movimentos_possiveis = getValidMoves(ru);
+			if (movimentos_possiveis.size()) {
+				for (int i = 0; i < movimentos_possiveis.size(); i++) {
+					makeMove(ru, movimentos_possiveis[i]);
+					int result = solve(ru, solucao, tabuleiros_verificados, nodes);
+					if (result) {
+						solucao.push_back(movimentos_possiveis[i]);
+						return 1;
+					}else {
+						vector<int> vector = getBoardsVariations(ru,tabuleiros_verificados);
+						tabuleiros_verificados.push_back(vector);
+						reverseMove(ru, movimentos_possiveis[i]);	
+					}
 				}
+				return 0;
+			}else {
+				return 0;
 			}
-			return 0;
 		}else {
 			return 0;
 		}
@@ -201,17 +257,17 @@ int solve(RESTAUM& ru, vector<MOVIMENTO>& solucao, int& nodes) {
 }
 
 int main() {
-	
+	auto t1 = chrono::high_resolution_clock::now();
 	int nodes = 0;
 	RESTAUM ru;
 	vector<MOVIMENTO> solucao;
+	vector<vector<int>> tabuleiros_verificados;
 	ru = getSave();
 	
 	cout << "ENCONTRANDO SOLUCAO: \n" << endl;
-
 	printGame(ru);
 
-	int result = solve(ru,solucao,nodes);
+	int result = solve(ru,solucao,tabuleiros_verificados,nodes);
 
 	if (result) {
 		cout << "SOLUCAO: \n" << endl;
@@ -231,6 +287,10 @@ int main() {
 		delete[] ru.tabuleiro[i];
 	}
 	delete[] ru.tabuleiro;
+
+	auto t2 = chrono::high_resolution_clock::now();
+	auto duration = chrono::duration_cast<chrono::microseconds>(t2 - t1).count();
+	cout << duration << endl;
 	
 
 	return 0;
